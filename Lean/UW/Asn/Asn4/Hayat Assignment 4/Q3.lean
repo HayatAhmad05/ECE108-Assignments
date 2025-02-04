b06e61660787a5389faeca8b282c4ed3037af49b
Robert Yin <robertyin2006@gmail.com>
2025-02-03 22:37:24 -0500

    done question 3?

--- Lean/UW/Asn/Asn4/Hayat Assignment 4/Q3.lean
+++ Lean/UW/Asn/Asn4/Hayat Assignment 4/Q3.lean
@@ -21,19 +21,26 @@ Def Q3 := Proof
     Asm 5: ∀ (x : Ty), ∃ (y : Ty), p x y
     Asm 6: ∃ (x : Ty), ∀ (y : Ty), p x y
     ⊢ ∃ (x y : Ty), p x y ∧ q x y
+
     By ExistsElim Asm 1
     Asm 7: Var (y1 : Ty)
     Asm 8: ∀ (x : Ty), p x y1 → q x y1
+
     By ExistsElim Asm 6
     Asm 9: Var (x1 : Ty)
     Asm 10: ∀ (y : Ty), p x1 y
+
     By ForallElim Asm 2 Using y=y1, x=x1
     Asm 11: p x1 y1 → ∃ (x : Ty), q x y1
+
     By ImplElim Asm 11
     Asm 12: p x1 y1
     ⊢ ∃ (x : Ty), q x y1
+
     By ExistsElim Goal Using x=x1
     ⊢ q x1 y1
+
     By ForallElim Asm 8 Using x=x1, Asm 12
     Asm 13: q x1 y1
+
     QED Asm 13
--- Lean/UW/Asn/Asn4/Q3.lean
+++ Lean/UW/Asn/Asn4/Q3.lean
@@ -21,3 +21,40 @@ Def Q3 := Proof
     Asm 5: ∀ (x : Ty), ∃ (y : Ty), p x y
     Asm 6: ∃ (x : Ty), ∀ (y : Ty), p x y
     ⊢ ∃ (x y : Ty), p x y ∧ q x y
+
+    -- prove p x y (turn into p a b)
+    By ExistsElim Asm 6
+    Asm 7: (a : Ty)
+    Asm 8: ∀ (y : Ty), p a y
+
+    By ForallElim Asm 5 Using x = a
+    Asm 9: ∃ (y : Ty), p a y
+
+    By ExistsElim Asm 9
+    Asm 10: (b : Ty)
+    Asm 11: p a b
+
+    By ExistsElim Goal Using x = a, y = b
+    ⊢ p a b ∧ q a b
+
+    -- prove q a b
+
+    By ForallElim Asm 4 Using x = a
+    Asm 12: (∀ (y: Ty), ¬ (p a y)) ∨ ∀ (y : Ty), q a y
+
+    By OrElim Asm 12 {
+        Subproof 1: "assume lhs: ∀ (y: Ty), ¬ (p a y)"
+            Asm 1/1: ∀ (y: Ty), ¬ (p a y)
+            By ForallElim Asm 1/1 Using y = b
+            Asm 1/2: ¬ (p a b)
+            QED Contradiction Asm 11, Asm 1/2
+
+        --------------------------------------------
+        Subproof 2: "assume rhs: ∀ (y : Ty), q a y"
+            Asm 2/1: ∀ (y : Ty), q a y
+            By ForallElim Asm 2/1 Using y = b
+            Asm 2/2: q a b
+            By AndIntro Asm 11, Asm 2/2
+            Asm 2/3: p a b ∧ q a b
+            QED Asm 2/3
+    }
