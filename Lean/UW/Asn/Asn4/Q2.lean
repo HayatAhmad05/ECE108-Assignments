340e7bbc30d43072d4b63e7456b152ecfa5dde19
Robert Yin <robertyin2006@gmail.com>
2025-02-03 23:20:25 -0500

    question 2 and 4

--- Lean/UW/Asn/Asn4/Q2.lean
+++ Lean/UW/Asn/Asn4/Q2.lean
@@ -18,3 +18,28 @@ Def Q2 := Proof
   Asm 3: ∀ (x : Ty), r (g (g x)) → q (g x)
   Asm 4: ∀ (x: Ty), r x → r (g x)
   ⊢ ∀ (x : Ty), p x → ∃ (y: Ty), q y
+
+  By ImplElim Goal
+  Asm 5: ∀ (x : Ty), p x
+  ⊢ ∃ (y : Ty), q y
+
+  By ImplElim Asm 1 Using Asm 5
+  Asm 6: ∃ (x : Ty), r x
+
+  By ExistsElim Asm 6
+  Asm 7: Var (a : Ty)
+  Asm 8: r a
+
+  By ForallElim Asm 4 Using x = a, Asm 8
+  Asm 9: r (g a)
+
+  By ForallElim Asm 3 Using g x = a
+  Asm 10: r (g a) → q (a)
+
+  By ImplElim Asm 10 Using Asm 9
+  Asm 11: q a
+
+  By ExistsElim Goal Using y = a
+  ⊢ q a
+
+  QED Asm 11
